18011879be7387892375bb6aaa04449ecb38a946
ReNnAn-ds <rennands@outlook.com>
2021-11-22 19:48:35 +0300

    add tests for jdbc, jpa, datajpa

---
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceDataJpaTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service;
+
+
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("datajpa")
+public class UserServiceDataJpaTest extends AbstractUserServiceTest {
+
+}
